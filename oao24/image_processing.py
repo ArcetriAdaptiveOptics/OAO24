4b1c5c10d8c158d81e79c0ad5e9a16a49b8bab49
Lorenzo Busoni <lbusoni@gmail.com>
2024-09-26 11:09:44 +0200

    fwhm_estimate added

---
+++ oao24/image_processing.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from oao24 import package_data
+
+
+def make_master_image(raw_data_cube, background_image):
+    ''' 
+    Compute the master image from a raw cube data and the background image.
+    
+    1. Dark image is subtracted to each image of the raw cube
+    2. Cube is cumulated (without shift and add)
+    
+    Dark_image must have the same camera setting
+    (i.e. exposure and filter) of each image of the raw cube
+    '''
+
+    Nframes = raw_data_cube.shape[-1]
+
+    # display the background image
+    plt.figure()
+    plt.imshow(background_image, vmin=0, vmax=2000)
+    # plt.imshow(background_image)
+    plt.colorbar(label='ADU')
+    plt.title("Background image")
+    print_roi_mean_values(background_image, label='Background')
+
+    # display one frame of the raw data cube
+    plt.figure()
+    plt.imshow(raw_data_cube[:, :, 0], vmin=0, vmax=2000)
+    plt.colorbar(label='ADU')
+    plt.title("Raw data image #0")
+    print_roi_mean_values(raw_data_cube[:, :, 0], label='Raw image #0')
+
+    # subtracting background from raw data
+    # make sure new array is float !!! to avoid integer overflows
+    background_subtracted_data_cube = np.zeros(
+        raw_data_cube.shape, dtype=float)
+    for frame in np.arange(Nframes):
+        background_subtracted_data_cube[:, :,
+                                        frame] = raw_data_cube[:, :, frame] - background_image
+
+    # Computing master image
+    # Sum along the NDIT dimension to obtain a single (512,640) image
+    # Must know total integration time
+    #
+    # Advanced: shift & add. Detects every image's maximum and shift
+    # every image before stacking to eliminate residual tip-tilt
+    ###########
+    master_image = background_subtracted_data_cube.sum(axis=-1)
+    print_roi_mean_values(master_image, label='Master image')
+
+    #########
+    # Check background subtraction is ok.
+    # Dark area should be around 0
+    ######
+    plt.figure()
+    plt.imshow(master_image, vmin=-10, vmax=100)
+    plt.title('Master image (linear scale, clipped)')
+    plt.colorbar()
+
+    #########
+    # Display image
+    # Dark area should be around 0
+    ######
+    plt.figure()
+    arr = master_image
+    plt.imshow(np.log10(arr-np.median(arr)+1), cmap='inferno')
+    plt.title('Master image (log scale)')
+    plt.colorbar()
+
+    return master_image
+
+
+def print_roi_mean_values(image, label=''):
+    '''
+    Print mean values of 4 Region Of Interest in the corners 
+    of the image.
+    Return average value in the ROIs
+    We use it as background value indicator
+    '''
+    bkg_roi1 = image[50:100, 50:100].mean()
+    bkg_roi2 = image[50:100, -100:-50].mean()
+    bkg_roi3 = image[-100:-50, 50:100].mean()
+    bkg_roi4 = image[-100:-50, -100:-50].mean()
+    bkg = np.mean([bkg_roi1, bkg_roi2, bkg_roi3, bkg_roi4])
+    print("%s : ROIs mean values %g %g %g %g - Average %g ADU" %
+          (label, bkg_roi1, bkg_roi2, bkg_roi3, bkg_roi4, bkg))
+    return bkg
--- oao24/package_data.py
+++ oao24/package_data.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from pathlib import Path
 ROOT_DIR_KEY = 'OAO24_ROOT_DIR'
 
@@ -25,3 +26,40 @@ def tuto2_folder():
 
 def tuto3_folder():
     return data_root_dir() / "tuto3"
+
+
+class InfraredExampleData():
+
+    @staticmethod
+    def get_open_loop_data_cube():
+        image = np.load(tuto3_folder() / "ID_105.npy")
+        return np.atleast_3d(image)
+
+    @staticmethod
+    def get_camera_dark_data():
+        return np.load(tuto3_folder() / "ID_110.npy")
+
+    @staticmethod
+    def get_close_loop_data_cube():
+        return np.load(tuto3_folder() / "ID_109_red_16.npy")
+
+
+class VisibleExampleData():
+
+    @staticmethod
+    def get_open_loop_data_cube():
+        with np.load(tuto3_folder() / "ORCA_478.npz") as data:
+            image = data['arr_0']
+        return np.atleast_3d(image)
+
+    @staticmethod
+    def get_camera_dark_data():
+        with np.load(tuto3_folder() / "ORCA_494.npz") as data:
+            image = data['arr_0']
+        return np.atleast_3d(image)
+
+    @staticmethod
+    def get_close_loop_data_cube():
+        with np.load(tuto3_folder() / "ORCA_477.npz") as data:
+            image = data['arr_0']
+        return np.atleast_3d(image)
